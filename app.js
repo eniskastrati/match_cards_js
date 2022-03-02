09f3217669b242f3d17dc0bfc9ccecd2b4ab5130
eniskastrati <eniskastrati18@gmail.com>
2022-03-02 15:29:28 +0100

    Created the card array

---
+++ app.js
@@ -0,0 +1,50 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'assets/images/fires.png',
+    },
+    {
+        name: 'chesseburger',
+        img: 'assets/images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'assets/images/hotdog.png',
+    },
+    {
+        name: 'ice-crean',
+        img: 'assets/images/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'assets/images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'assets/images/pizza.png',
+    },
+    {
+        name: 'fries',
+        img: 'assets/images/fires.png',
+    },
+    {
+        name: 'chesseburger',
+        img: 'assets/images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'assets/images/hotdog.png',
+    },
+    {
+        name: 'ice-crean',
+        img: 'assets/images/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'assets/images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'assets/images/pizza.png',
+    }
+];
